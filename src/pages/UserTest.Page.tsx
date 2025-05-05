1a2dcf8877adec65535599e1ff1f6f0bac5f9d50
Gavin Jin <gavinjin62@gmail.com>
2025-05-04 18:22:53 -0700

    feat: add protected fetching to backed

--- src/pages/UserTest.Page.tsx
+++ src/pages/UserTest.Page.tsx
@@ -1,10 +1,34 @@
-import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import { useEffect, useState } from 'react';
+import { SignedIn, SignedOut, useAuth } from '@clerk/clerk-react';
+import axios from 'axios';
 
 export default function UserTest() {
+  const { getToken } = useAuth();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchTestData = async () => {
+      try {
+        const token = await getToken();
+        const res = await axios.get('http://localhost:3000/api/user/test', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setData(res.data);
+      } catch (err) {
+        console.error('Failed to fetch protected data', err);
+      }
+    };
+
+    fetchTestData();
+  }, [getToken]);
+
   return (
     <>
       <SignedIn>
         <h1>Test protected page</h1>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>loading data</p>}
       </SignedIn>
       <SignedOut>
         <h1>Must be signed in</h1>
