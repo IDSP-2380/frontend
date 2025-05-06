de0e76f3862d7510cfeb1c157e79f858312dd92b
Gavin <gavinjin62@gmail.com>
2025-05-06 13:26:23 -0700

    add editcard props

--- src/components/Cards/EditCard.tsx
+++ src/components/Cards/EditCard.tsx
@@ -1,16 +1,17 @@
-import { Box, Flex } from '@mantine/core';
+import { Box, Flex, Text } from '@mantine/core';
 import EditClasses from './EditCard.module.css';
 
-export default function EditCard() {
+interface EditCardProps {
+  linkNumber: String;
+  linkContent: string;
+}
+
+export default function EditCard({ linkNumber, linkContent }: EditCardProps) {
   return (
     <Box>
       <Flex>
-        <h2 className={EditClasses.numberText}>1</h2>
-        <Box className={EditClasses.cardBorder}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, est nam! Eaque ipsum,
-          aperiam, illum voluptates tempora, tenetur hic ad aut molestiae qui sint consequuntur fuga
-          veniam nostrum adipisci aliquam?
-        </Box>
+        <Text className={EditClasses.numberText}>{linkNumber}</Text>
+        <Box className={EditClasses.cardBorder}>{linkContent}</Box>
       </Flex>
     </Box>
   );
--- src/pages/Editor.page.tsx
+++ src/pages/Editor.page.tsx
@@ -2,11 +2,36 @@ import EditCard from '@/components/Cards/EditCard';
 import TextEditor from '@/components/TextEditor/TextEditor';
 
 export function EditorPage() {
+  const testData = [
+    {
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus incidunt ipsa soluta delectus debitis excepturi provident neque numquam, minus omnis nihil, sit praesentium aperiam! Consequatur provident accusamus eum sequi atque.',
+    },
+    {
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus incidunt ipsa soluta delectus debitis excepturi provident neque numquam, minus omnis nihil, sit praesentium aperiam! Consequatur provident accusamus eum sequi atque.',
+    },
+    {
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus incidunt ipsa soluta delectus debitis excepturi provident neque numquam, minus omnis nihil, sit praesentium aperiam! Consequatur provident accusamus eum sequi atque.',
+    },
+    {
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus incidunt ipsa soluta delectus debitis excepturi provident neque numquam, minus omnis nihil, sit praesentium aperiam! Consequatur provident accusamus eum sequi atque.',
+    },
+    {
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus incidunt ipsa soluta delectus debitis excepturi provident neque numquam, minus omnis nihil, sit praesentium aperiam! Consequatur provident accusamus eum sequi atque.',
+    },
+  ];
+
   return (
     <>
       <h1>Story title goes here or some shit</h1>
 
-      <EditCard />
+      {testData.map((story, index) => {
+        return <EditCard linkNumber={(index + 1).toString()} linkContent={story.content} />;
+      })}
 
       <TextEditor />
     </>
