8b5f3a459a18f4d18645df0c17dccbfe46fd2412
Gavin Jin <gavinjin62@gmail.com>
2025-05-14 22:10:13 -0700

    change primary button color

--- src/pages/SignIn.page.tsx
+++ src/pages/SignIn.page.tsx
@@ -11,7 +11,21 @@ export default function SignInPage() {
       <SignedOut>
         <Box className={ClerkStyle.mainWrapper}>
           <Flex justify={'center'} align={'center'}>
-            <SignIn routing="hash" signUpUrl={import.meta.env.VITE_CLERK_SIGN_UP_URL} />
+            <SignIn
+              routing="hash"
+              signUpUrl={import.meta.env.VITE_CLERK_SIGN_UP_URL}
+              appearance={{
+                elements: {
+                  formButtonPrimary: 'bg-indigo-600 hover:bg-indigo-700 text-white font-semibold',
+                  card: 'w-[500px] max-w-full p-8 shadow-lg rounded-lg',
+                  headerTitle: 'text-2xl font-bold',
+                  footerActionText: 'text-sm text-gray-500',
+                },
+                variables: {
+                  colorPrimary: '#2d4ab4',
+                },
+              }}
+            />
           </Flex>
         </Box>
         {/* <SignIn
