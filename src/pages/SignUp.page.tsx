5261d7ab8f80c41e61f08b39b02fbbd4656e2b98
Gavin Jin <gavinjin62@gmail.com>
2025-05-14 21:48:29 -0700

    center login card and add logo

--- src/pages/SignUp.page.tsx
+++ src/pages/SignUp.page.tsx
@@ -1,11 +1,17 @@
 import { SignedIn, SignedOut, SignUp } from '@clerk/clerk-react';
 import { Navigate, useLoaderData } from 'react-router-dom';
+import { Box, Flex } from '@mantine/core';
+import ClerkStyle from '../styles/Clerk.module.css';
 
 export default function SignUpPage() {
   return (
     <>
       <SignedOut>
-        <SignUp routing="hash" signInUrl={import.meta.env.VITE_CLERK_SIGN_IN_URL} />
+        <Box className={ClerkStyle.mainWrapper}>
+          <Flex justify={'center'} align={'center'}>
+            <SignUp routing="hash" signInUrl={import.meta.env.VITE_CLERK_SIGN_IN_URL} />
+          </Flex>
+        </Box>
       </SignedOut>
       <SignedIn>
         <Navigate to="/" />
